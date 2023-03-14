26d1b134f73bfed13ea8ee0ea0fec5b763077d88
Erickkirui <erickkirui653@gmail.com>
2023-03-14 06:12:36 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
+// a multiple of 62
 
-// Write your code here
+    let num1 = 31
+    let num2 = 2
+    let multiply =""
+   multiply= num1 * num2;
+    
+
+
+
+
+//random number greater than 0
+let random = 0
+random++
+
+// numbers with the reminder of 4
+
+let mod = ""
+let num3 = 24
+let num4 = 5
+mod = num3 % num4
+console.log(mod);
+
+let max = [4, 13, 10, 3, 8, 6, 20, 20, 14];
+let highest = max[0];   // Initialize highest to the first element in the array
+for (let i = 1; i < max.length; i++) {
+  if (max[i] > highest) {
+    highest = max[i];
+  }
+}
